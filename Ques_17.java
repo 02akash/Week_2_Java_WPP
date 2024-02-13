c6779a74c31e666a793fcea7c2bc32228e14295a
Akash soni <94229771+02akash@users.noreply.github.com>
2024-02-13 18:16:40 +0530

    Create Ques_17.java

---
+++ Ques_17.java
@@ -0,0 +1,25 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class arr09 {
+    public static void main(String[] args) {
+        
+        throw IOException {
+
+            FileWriter fw = new FileWriter("/Users/akashsoni/Desktop/DSA_Pw");
+
+            fw.write("I am the Earth \n");
+            fw.write("I am the Forest Green \n");
+            fw.write("I am the Earth \n");
+            fw.write("I am the Earth \n");
+
+            fw.flush();
+
+            fw.close();
+
+            System.out.println("File Prepared Successfully!");
+        }
+    }
+    
+}
